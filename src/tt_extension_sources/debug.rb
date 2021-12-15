478a7195f3aea3c932faf9b54fe4eaae8df469f4
Thomas Thomassen <thomas@thomthom.net>
2021-12-15 21:56:00 +0100

    Fix RuboCop warnings.

--- src/tt_extension_sources/debug.rb
+++ src/tt_extension_sources/debug.rb
@@ -1,7 +1,5 @@
 module TT::Plugins::ExtensionSources
 
-  # TT::Plugins::ExtensionSources::SETTINGS.debug = true
-
   # @note Debug method to reload the plugin.
   #
   # @example
@@ -11,8 +9,8 @@ module TT::Plugins::ExtensionSources
   def self.reload
     original_verbose = $VERBOSE
     $VERBOSE = nil
-    load __FILE__
-    pattern = File.join(__dir__, '**/*.rb')
+    load __FILE__ # rubocop:disable SketchupSuggestions/FileEncoding
+    pattern = File.join(__dir__, '**/*.rb') # rubocop:disable SketchupSuggestions/FileEncoding
     Dir.glob(pattern).each { |file| load file }.size
   ensure
     $VERBOSE = original_verbose

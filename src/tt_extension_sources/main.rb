e145effd973bce58c1e847714c89e900a737cae8
Thomas Thomassen <thomas@thomthom.net>
2021-12-15 22:09:39 +0100

    Flesh out ExtensionSource class.

--- src/tt_extension_sources/main.rb
+++ src/tt_extension_sources/main.rb
@@ -1,9 +1,50 @@
 require 'sketchup.rb'
 
+require 'json'
+
+require 'tt_extension_sources/debug'
+
 module TT::Plugins::ExtensionSources
+
+  class ExtensionSource
+
+    attr_accessor :path, :enabled
+
+    # @param [String] path
+    # @param [Boolean] enabled
+    def initialize(path:, enabled: true)
+      @data = {
+        path: path,
+        enabled: enabled,
+      }
+    end
+
+    # @return [Hash]
+    def to_hash
+      @data.dup
+    end
+
+    # @return [Hash]
+    def as_json(options={})
+      # https://stackoverflow.com/a/40642530/486990
+      to_hash
+    end
+
+    # @return [String]
+    def to_json(*args)
+      @data.to_json(*args)
+    end
+
+  end # class
 
   def self.open_extension_sources_dialog
     puts 'Extension Sources...'
+    [
+      {
+        path: 'C:/Users/Thomas/SourceTree/TrueBend/src',
+        enabled: true,
+      }
+    ]
   end
 
   unless file_loaded?(__FILE__)

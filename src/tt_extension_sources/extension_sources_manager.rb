4f8ce926fb8a34246be81e6e7a2be7f7d146e85c
Thomas Thomassen <thomas@thomthom.net>
2021-12-29 21:17:07 +0100

    Add/remove $LOAD_PATH and require sources.

--- src/tt_extension_sources/extension_sources_manager.rb
+++ src/tt_extension_sources/extension_sources_manager.rb
@@ -9,16 +9,18 @@ module TT::Plugins::ExtensionSources
       @data = from_hash(dummy_extension_sources)
     end
 
-    # @param [String] path
+    # @param [String] source_path
     # @return [ExtensionSource, nil]
-    def add(path)
-      return nil if include_path?(path)
+    def add(source_path)
+      raise "Path doesn't exist: #{source_path}" unless File.exist?(source_path)
 
-      source = ExtensionSource.new(path: path)
+      return nil if include_path?(source_path)
+
+      source = ExtensionSource.new(path: source_path)
       @data << source
 
-      # TODO: Add to load path.
-      # TODO: Require root files.
+      add_load_path(source.path)
+      require_sources(source.path)
 
       source
     end
@@ -27,10 +29,11 @@ module TT::Plugins::ExtensionSources
     # @return [ExtensionSource, nil]
     def remove(path_id)
       source = find_by_path_id(path_id)
+      raise IndexError, "source path id #{path_id} not found" unless source
 
-      # TODO: Remove from load path.
+      @data.delete_if { |item| item.path == source.path }
+      remove_load_path(source.path)
 
-      @data.delete(source)
       source
     end
 
@@ -45,11 +48,13 @@ module TT::Plugins::ExtensionSources
       # TODO: Use custom errors.
       raise "path '#{path}' already exists" if path && include_path?(path)
 
+      remove_load_path(source.path) if path
+
       source.path = path unless path.nil?
       source.enabled = enabled unless enabled.nil?
 
-      # TODO: Update load path.
-      # TODO: Require root files.
+      add_load_path(source.path) if path
+      require_sources(source.path)
 
       source
     end
@@ -94,6 +99,30 @@ module TT::Plugins::ExtensionSources
     end
 
     private
+
+    # @param [String] source_path
+    # @return [Array<String>]
+    def require_sources(source_path)
+      pattern = "#{source_path}/*.rb"
+      Dir.glob(pattern).each { |path|
+        Sketchup.require(path)
+      }.to_a
+    end
+
+    # @param [String] source_path
+    # @return [Boolean]
+    def add_load_path(source_path)
+      return false if $LOAD_PATH.include?(source_path)
+
+      $LOAD_PATH << source_path
+      true
+    end
+
+    # @param [String] source
+    # @return [Boolean]
+    def remove_load_path(source_path)
+      !$LOAD_PATH.delete(source_path).nil?
+    end
 
     # @param [Array<Hash>] data
     # @return [Array<ExtensionSource>]

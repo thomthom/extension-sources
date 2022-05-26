5be7d36469b1586d53df50e387c34ff71be40461
Thomas Thomassen <thomas@thomthom.net>
2022-05-26 22:02:27 +0200

    Base for selecting items in the scanner dialog.

--- src/tt_extension_sources/view/ui/js/extension_sources_scanner.js
+++ src/tt_extension_sources/view/ui/js/extension_sources_scanner.js
@@ -2,6 +2,7 @@ let app = new Vue({
   el: '#app',
   data: {
     filter: "",
+    last_selected_index: null,
     sources: [],
   },
   computed: {
@@ -20,7 +21,7 @@ let app = new Vue({
     },
     selected() {
       return this.sources.filter((source) => {
-        return source.enabled; // TODO: source.selected
+        return source.selected;
       });
     }
   },
@@ -32,7 +33,36 @@ let app = new Vue({
       this.filter = "";
     },
     update(sources) {
+      // Inject additional properties that only the Vue app cares about.
+      // For Vue's reactor to pick up the changes they must be added to
+      // the `sources` data before assigning to the Vue app's data.
+      for (const item of sources) {
+        item.selected = false;
+      }
       this.sources = sources;
+    },
+    select(event, source, index) {
+      console.log('select', source, source.source_id, 'selected:', source.selected, event);
+      // TODO: Mouse+drag to multi-select.
+      // Clear existing selection unless Ctrl is pressed.
+      if (!event.ctrlKey) { // TODO: macOS key handling.
+        console.log('> clear-select');
+        for (const item of this.sources) {
+          item.selected = false;
+        }
+      }
+      if (event.shiftKey && this.last_selected_index !== null) { // TODO: macOS key handling.
+        console.log('> multi-select');
+        const min = Math.min(this.last_selected_index, index);
+        const max = Math.max(this.last_selected_index, index);
+        for (let i = min; i <= max; ++i) {
+          this.sources[i].selected = true;
+        }
+      } else {
+        console.log('> single-select');
+        source.selected = !source.selected;
+      }
+      this.last_selected_index = index;
     },
     accept() {
       sketchup.accept(this.selected);

5dbf50ce236d0bff83ef0e690c8984f66b7277b5
Thomas Thomassen <thomas@thomthom.net>
2022-06-10 16:22:48 +0200

    Stable partition sorting.

--- research/reorder.rb
+++ research/reorder.rb
@@ -2,7 +2,7 @@ require 'json'
 
 Item = Struct.new(:name, :index, :selected, keyword_init: true)
 
-target_index = 5
+target_index = ARGV[0]&.to_i || 5
 
 items = [
   Item.new(name: 'item0', index: 0, selected: false), # 0
@@ -13,45 +13,71 @@ items = [
   Item.new(name: 'item5', index: 5, selected: false), # 5
   Item.new(name: 'item6', index: 6, selected: false), # 6
 ]
-expected = [0, 1, 3, 5, 2, 4, 6].map { |i| items[i] }
+expected = [0, 1, 3, 2, 4, 5, 6].map { |i| items[i] }
 
 puts
 puts "Original:"
 items.each { |item| p item.to_h }
 
-first_selected_index = items.index { |item| item.selected } || 0
-last_selected_index = items.rindex { |item| item.selected } || [item.size - 1, 0].max
-lower_bound_index = [first_selected_index, target_index].min
-upper_bound_index = [last_selected_index, target_index].max
-movable_range = (lower_bound_index..upper_bound_index)
+# item0
+# item1
+# item2 *
+# item3 *
+# item4 <- insert: (after)
+# item5 *
+# item6
+#
+# item0 <- first: 0
+# item1
+# item2 *
+# item3 *
+# item4 <- insert: 4 (after)
+# item5 *
+# item6
+# ----- <- last: 7
+#
+# stable_partition [first, insert)
+# stable_partition [insert, last)
+#
+# item0
+# item1
+# item2 *
+# item3 *
+# item4 <- insert: (after)
+# item5 *
+# item6
+#
+# Ruby Ranges:
+# (0..6) == [0..6]  <- inclusive last
+# (0...6) == [0..6) <- exclusive last
+
+upper = (0...target_index)
+sorted = items.sort_by { |item|
+  in_upper = upper.include?(item.index)
+  to_top_of_partition = if in_upper
+    item.selected ? 1 : 0
+  else
+    item.selected ? 0 : 1
+  end
+  primary = in_upper ? 0 : 1
+  secondary = to_top_of_partition
+  tertiary = item.index
+  [primary, secondary, tertiary]
+}
 
 puts
-p [:target_index, target_index]
-p [:first_selected_index, first_selected_index]
-p [:last_selected_index, last_selected_index]
-p [:lower_bound_index, lower_bound_index]
-p [:upper_bound_index, upper_bound_index]
-p [:movable_range, movable_range]
-
-SORT_MOVE_UP = -1
-SORT_NO_MOVE = 0
-SORT_MOVE_DOWN = 1
-
-sorted = items.dup
-items.each { |item|
-  if item.selected
-    sorted.delete(item)
-    sorted.insert(target_index, item)
-  end
-}
+puts "Moved:"
+puts "#{items.select(&:selected).map(&:index)} to #{target_index}"
 
 puts
 puts "Actual:"
 sorted.each { |item| p item.to_h }
 
-puts
-puts "Expected:"
-expected.each { |item| p item.to_h }
+if ARGV.empty?
+  puts
+  puts "Expected:"
+  expected.each { |item| p item.to_h }
 
-puts
-puts "Match: #{sorted == expected}"
+  puts
+  puts "Match: #{sorted == expected}"
+end

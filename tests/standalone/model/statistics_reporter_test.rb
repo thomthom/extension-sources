c52fe02b2286f7447a37542ef16ac3a754674057
Thomas Thomassen <thomas@thomthom.net>
2023-04-05 22:13:55 +0200

    Add option to group timing reports to varying granularity of the SketchUp version.

--- tests/standalone/model/statistics_reporter_test.rb
+++ tests/standalone/model/statistics_reporter_test.rb
@@ -8,29 +8,26 @@ require 'tt_extension_sources/model/statistics_reporter'
 module TT::Plugins::ExtensionSources
   class StatisticsReporterTest < Minitest::Test
 
-    def test_report
-      path = File.join(__dir__, 'load-times.csv')
-      records = File.open(path) { |file|
+    # @param [String] records_csv
+    # @param [String] expected_json
+    def compare_report_data(records_csv, expected_json, **report_options)
+      records = File.open(records_csv) { |file|
         StatisticsCSV.new(io: file).read
       }
 
       stats = StatisticsReporter.new
-      report = stats.report(records)
+      report = stats.report(records, **report_options)
       assert_kind_of(Hash, report)
       # puts
       # puts JSON.pretty_generate(report)
       # puts
 
-      expected_path = File.join(__dir__, 'load-times.json')
+      expected_path = expected_json
       expected = JSON.parse(File.read(expected_path))
 
       # Enable to regenerate expected JSON data:
       # File.write(expected_path, JSON.pretty_generate(report))
 
-      # assert(report.key?('total'), "expected: 'total'")
-      # assert(report.key?('versions'), "expected: 'versions'")
-
-      # expected.each { |path, versions|
       expected.keys.each { |path|
         assert(report.key?(path), "expected: #{path}")
 
@@ -42,13 +39,38 @@ module TT::Plugins::ExtensionSources
           assert(report[path][:versions].key?(version), "expected: #{path}:#{version}")
 
           expected_data = data.transform_keys(&:to_sym)
-          # assert_kind_of(Statistics::Record, report[path][version])
-          # assert_equal(expected_data, report[path][version].to_h)
           assert_equal(expected_data, report[path][:versions][version])
         }
         assert_equal(versions.keys.sort, report[path][:versions].keys.sort)
       }
       assert_equal(expected.keys.sort, report.keys.sort)
+    end
+
+    def test_report_defaults
+      records_path = File.join(__dir__, 'load-times.csv')
+      expected_path = File.join(__dir__, 'load-times.json')
+      compare_report_data(records_path, expected_path)
+    end
+
+    def test_report_group_by_major_minor_patch
+      records_path = File.join(__dir__, 'load-times.csv')
+      expected_path = File.join(__dir__, 'load-times_major-minor-patch.json')
+      compare_report_data(records_path, expected_path,
+        group_by: StatisticsReporter::GROUP_BY_MAJOR_MINOR_PATCH)
+    end
+
+    def test_report_group_by_major_minor
+      records_path = File.join(__dir__, 'load-times.csv')
+      expected_path = File.join(__dir__, 'load-times_major-minor.json')
+      compare_report_data(records_path, expected_path,
+        group_by: StatisticsReporter::GROUP_BY_MAJOR_MINOR)
+    end
+
+    def test_report_group_by_major
+      records_path = File.join(__dir__, 'load-times.csv')
+      expected_path = File.join(__dir__, 'load-times_major.json')
+      compare_report_data(records_path, expected_path,
+        group_by: StatisticsReporter::GROUP_BY_MAJOR)
     end
 
   end # class
